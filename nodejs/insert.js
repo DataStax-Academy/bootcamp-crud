5c1a6b7a0b88da424cca9ed27df9115848b6e948
mando222 <eric.zietlow@datastax.com>
2019-08-05 17:58:17 -0400

    added in some connection code

--- nodejs/insert.js
+++ nodejs/insert.js
@@ -1,1 +1,9 @@
 // this is a insert statement in nodejs
+const ip_address = require('./ip_address')
+
+const dse = require('dse-driver');
+const client = new dse.Client({ contactPoints: [ip_address()], localDataCenter: 'datacenter1' });
+
+const query1 = 'SELECT * FROM users WHERE key = BOB';
+client.execute(query1)
+.then(result => console.log('User with email %s', result.rows[0].email));
--- nodejs/select.js
+++ nodejs/select.js
@@ -1,11 +1,12 @@
 // this is a select statement in nodejs
-// this is a insert statement in nodejs
-const dse = require('dse-driver');
-const client = new dse.Client({ contactPoints: [IPADDRESSHERE], localDataCenter: 'datacenter1' });
+const ip_address = require('./ip_address')
 
+const dse = require('dse-driver');
+const client = new dse.Client({ contactPoints: [ip_address()], localDataCenter: 'datacenter1' });
 
 const query1 = 'SELECT * FROM users WHERE key = BOB';
 client.execute(query1)
+.then(result => console.log('User with email %s', result.rows[0].email));
 
 
 const query2 = 'SELECT * FROM users WHERE key = ?';

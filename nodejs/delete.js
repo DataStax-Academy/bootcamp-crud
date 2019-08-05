3f995acd1abec566724fee12bb085b18856eb110
mando222 <eric.zietlow@datastax.com>
2019-08-05 18:02:59 -0400

    basic select and delete structure made"

--- nodejs/delete.js
+++ nodejs/delete.js
@@ -4,6 +4,6 @@ const ip_address = require('./ip_address')
 const dse = require('dse-driver');
 const client = new dse.Client({ contactPoints: [ip_address()], localDataCenter: 'datacenter1' });
 
-const query1 = 'SELECT * FROM users WHERE key = BOB';
+const query1 = 'DELETE FROM crud.users WHERE name = BOB';
 client.execute(query1)
 .then(result => console.log('User with email %s', result.rows[0].email));

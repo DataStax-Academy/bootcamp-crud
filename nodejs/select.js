e4480fdff7b522232e7e52d85e416197db263c40
mando222 <eric.zietlow@datastax.com>
2019-08-05 17:52:18 -0400

    untested select code

--- nodejs/select.js
+++ nodejs/select.js
@@ -1,1 +1,13 @@
 // this is a select statement in nodejs
+// this is a insert statement in nodejs
+const dse = require('dse-driver');
+const client = new dse.Client({ contactPoints: [IPADDRESSHERE], localDataCenter: 'datacenter1' });
+
+
+const query1 = 'SELECT * FROM users WHERE key = BOB';
+client.execute(query1)
+
+
+const query2 = 'SELECT * FROM users WHERE key = ?';
+client.execute(query2, [ 'someone' ])
+  .then(result => console.log('User with email %s', result.rows[0].email));

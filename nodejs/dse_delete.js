3ea0d0c575d477aaa0e7bc7c3957398cd69bf1c0
mando222 <eric.zietlow@datastax.com>
2019-08-06 15:33:27 -0400

    more correct connection

--- nodejs/dse_delete.js
+++ nodejs/dse_delete.js
@@ -1,9 +1,9 @@
-const init_connection = require('./ip_address')
+const connection = require('./ip_address')
 
-connection = init_connection()
+console.log(connection.client.connected)
 
 // this is a delete statement in nodejs
-const query1 = 'DELETE FROM crud.users WHERE name = BOB';
-connection.client.execute(query1)
-.then(result => console.log(result))
-.catch((error) => {console.log(error)});
+connection.client.execute('DELETE * FROM crud.users WHERE first_name = ?', ['Bob'])
+.then(console.log('Success'))
+.catch(error => console.log(error.message));
+process.exit()
--- nodejs/ip_address.js
+++ nodejs/ip_address.js
@@ -3,10 +3,15 @@ const dse = require('dse-driver');
 function init_connection(){
     var connection = {}
     connection.ip_address = '127.0.0.1';
+    connection.dc = 'Cassandra'
     connection.path_to_creds=''
-    connection.client = new dse.Client({ contactPoints: [connection.ip_address], localDataCenter: 'Cassandra' });
+    connection.client = new dse.Client({ 
+        contactPoints: [connection.ip_address],
+        localDataCenter: connection.dc 
+    });
     return connection
 }
 
+connection = init_connection()
 
-module.exports = init_connection;
+module.exports = connection;

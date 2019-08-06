facebf15369d33f2e63e4cf49d47190f639a1104
mando222 <eric.zietlow@datastax.com>
2019-08-06 11:56:36 -0400

    changed some python file names due to conflict

--- python/dse_delete.py
+++ python/dse_delete.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 # # this is a delete statement in python
-from dse.cluster import Cluster, ExecutionProfile, EXEC_PROFILE_DEFAULT
-from dse.query import tuple_factory
+# from dse.cluster import Cluster, ExecutionProfile, EXEC_PROFILE_DEFAULT
+# from dse.query import tuple_factory
 
 # cluster = Cluster([127.0.0.1])
 # session = cluster.connect()

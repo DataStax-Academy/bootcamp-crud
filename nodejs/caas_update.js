0e8a5250405945fa8b4be498e26e47b61e9cdf09
mando222 <eric.zietlow@datastax.com>
2019-08-07 12:06:24 -0400

    restructured caas_update.js a bit

--- nodejs/caas_update.js
+++ nodejs/caas_update.js
@@ -1,7 +1,11 @@
 const connection = require('./ip_address')
 
 // this is a update statement in nodejs
-connection.client.execute('UPDATE crud.users SET age = ? WHERE first_name = ?', [40,'Bob'])
+connection.client.execute(
+    'UPDATE crud.users SET age = ? WHERE first_name = ?',
+    [40,'Bob'],
+    { prepare : true }
+)
 .then(console.log('Success'))
 .catch(error => console.log(error.message));
 // process.exit()

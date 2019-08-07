417ba5086786f6c227f903e56500fdca0a979851
mando222 <eric.zietlow@datastax.com>
2019-08-07 12:08:58 -0400

    node stuff fully working here with old schema

--- nodejs/caas_delete.js
+++ nodejs/caas_delete.js
@@ -1,6 +1,9 @@
 const connection = require('./ip_address')
 
 // this is a delete statement in nodejs
-connection.client.execute('DELETE * FROM crud.users WHERE first_name = ?', ['Bob'])
+connection.client.execute(
+    'DELETE * FROM crud.users WHERE first_name = ?',
+    ['Bob']
+)
 .then(console.log('Success'))
 .catch(error => console.log(error.message));

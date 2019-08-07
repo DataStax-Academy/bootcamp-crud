725d974e280659dc35419494f348155bca786293
mando222 <eric.zietlow@datastax.com>
2019-08-07 12:10:22 -0400

    fixed a bug in delete

--- nodejs/caas_delete.js
+++ nodejs/caas_delete.js
@@ -2,7 +2,7 @@ const connection = require('./ip_address')
 
 // this is a delete statement in nodejs
 connection.client.execute(
-    'DELETE * FROM crud.users WHERE first_name = ?',
+    'DELETE FROM crud.users WHERE first_name = ?',
     ['Bob']
 )
 .then(console.log('Success'))
